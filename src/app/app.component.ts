6038f04d506c7ba7baabfbb8cf0093040496ab9e
Fyasco <alan.choufa@gmail.com>
2023-06-20 11:12:57 +0200

    chore: cleaning starting file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'demo-rxjs-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `test`,
+ 
 })
 export class AppComponent {
   title = 'demo-rxjs';
